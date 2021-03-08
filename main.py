9ccc0bdeac4c01783a0446e63e3afef00313d6ae
Martin Tichem <martint@topdesk.com>
2021-03-09 00:46:56 +0100

    Add login option

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os, os.path
-from flask import Flask, render_template
+from flask import Flask, request, make_response, render_template
 from jinja_markdown import MarkdownExtension
 
 app = Flask(__name__)
@@ -8,11 +8,30 @@ app.jinja_env.add_extension(MarkdownExtension)
 @app.route("/")
 def home():
     puzzle_list=os.listdir("templates/puzzles")
-    return render_template("index.html", puzzles=puzzle_list)
+    name = request.cookies.get('userID')
+    return render_template("index.html", username=name, puzzles=puzzle_list)
+
+@app.route('/login')
+def index():
+   return render_template('login.html')
+
+@app.route('/setuser', methods = ['POST', 'GET'])
+def setuser():
+   if request.method == 'POST':
+    user = request.form['nm']
+    resp = make_response(render_template('loginconfirm.html'))
+    resp.set_cookie('userID', user)
+    return resp
+
+@app.route('/getuser')
+def getuser():
+   name = request.cookies.get('userID')
+   return '<p>username: '+name+'</p>'
 
 @app.route("/puzzle/<number>")
 def puzzle(number):
-    return render_template("puzzle.html", puzzle_number=number)
+    name = request.cookies.get('userID')
+    return render_template("puzzle.html", username=name, puzzle_number=number)
 
 @app.route("/puzzleinput/<number>")
 def puzzle_input(number):
