fdc893361f55ca92644d51d16466f8c3a877d9a4
Martin Tichem <martint@topdesk.com>
2021-03-08 19:24:30 +0100

    Add puzzle input pages

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ app.jinja_env.add_extension(MarkdownExtension)
 
 @app.route("/")
 def home():
-    puzzles=os.listdir("templates/puzzles")
-    return render_template("index.html", puzzles=puzzles)
+    puzzle_list=os.listdir("templates/puzzles")
+    return render_template("index.html", puzzles=puzzle_list)
 
 @app.route("/puzzle/<number>")
 def puzzle(number):
     return render_template("puzzle.html", puzzle_number=number)
+
+@app.route("/puzzleinput/<number>")
+def puzzle_input(number):
+    return render_template("puzzleinput.html", puzzle_number=number)
 
 if __name__ == "__main__":
     app.run()
