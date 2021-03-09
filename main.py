b0a38115d6170675384d9c769099a06ec110e38b
Martin Tichem <martint@topdesk.com>
2021-03-09 01:58:09 +0100

    Add input and check

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ from jinja_markdown import MarkdownExtension
 app = Flask(__name__)
 app.jinja_env.add_extension(MarkdownExtension)
 
+answers = {
+    "1": "(-107,35)",
+    "2": "foo",
+    "3": "foo",
+    "4": "foo",
+    "5": "foo",
+    "6": "foo",
+}
+
 @app.route("/")
 def home():
     puzzle_list=os.listdir("templates/puzzles")
@@ -13,11 +22,10 @@ def home():
 
 @app.route('/login')
 def index():
-   return render_template('login.html')
+    return render_template('login.html')
 
 @app.route('/setuser', methods = ['POST'])
 def setuser():
-   if request.method == 'POST':
     user = request.form['nm']
     resp = make_response(render_template('loginconfirm.html'))
     resp.set_cookie('userID', user, secure=True)
@@ -25,8 +33,8 @@ def setuser():
 
 @app.route('/getuser')
 def getuser():
-   name = request.cookies.get('userID')
-   return '<p>username: '+name+'</p>'
+    name = request.cookies.get('userID')
+    return '<p>username: '+name+'</p>'
 
 @app.route("/puzzle/<number>")
 def puzzle(number):
@@ -36,6 +44,13 @@ def puzzle(number):
 @app.route("/puzzleinput/<number>")
 def puzzle_input(number):
     return render_template("puzzleinput.html", puzzle_number=number)
+
+@app.route("/submitanswer/<number>", methods = ['POST'])
+def submit_answer(number):
+    name = request.cookies.get('userID')
+    answered = request.form['answer_input']
+    template = "correct.html" if (answers.get(number) == answered) else "incorrect.html"
+    return make_response(render_template(template, puzzle_number=number, username=name))
 
 if __name__ == "__main__":
     app.run()
