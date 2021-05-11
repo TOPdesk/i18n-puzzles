d21b09f372ffaf513466755f05b0db48633f6a61
Martijn van Iersel <martijni@topdesk.com>
2021-05-11 10:45:50 +0200

    Store scores in SQLite

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY templates /usr/src/app/templates/
 COPY main.py /usr/src/app/
 
+VOLUME /srv/code-challenge
+ENV DATABASE_URI sqlite:////srv/code-challenge/scores.sqlite
 EXPOSE 8080
 
 CMD [ "python", "main.py" ]
--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import os, os.path
 from flask import Flask, request, make_response, render_template
 from jinja_markdown import MarkdownExtension
 from datetime import datetime
+from sqlalchemy.sql.expression import select
 from waitress import serve
 import json
 import jinja2
+from flask_sqlalchemy import SQLAlchemy
 
 # Default puzzle directory may be overridden with env. variable
 puzzle_path = os.environ.get('PUZZLE_PATH') or "coding_challenge"
+db_uri = os.environ.get('DATABASE_URL') or 'sqlite:///scores.sqlite'
 
 # puzzle_path + templates directory are both scanned for jinja content
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+
 app.jinja_env.add_extension(MarkdownExtension)
 my_loader = jinja2.ChoiceLoader([
         app.jinja_loader, jinja2.FileSystemLoader([puzzle_path]),
@@ -23,9 +28,25 @@ with open(config_file, encoding="utf-8") as infile:
     puzzle_data = json.load(infile)
 answers = puzzle_data["answers"]
 
+db = SQLAlchemy(app)
+
+from sqlalchemy import Column, DateTime, String, Integer, func
+
+class Score(db.Model):
+    id = Column('id', Integer, primary_key = True)
+    player_name = Column(String(100))
+    puzzle_id = Column(Integer)
+    timestamp = Column(DateTime, default=func.now())
+
+    def __init__(self, player_name, puzzle_id):
+        self.player_name = player_name
+        self.puzzle_id = puzzle_id
+
+db.create_all()
+
 accept_new_answers = True
+
 players = []
-scores = {}
 
 def get_puzzle_description(puzzle_number):
     return puzzle_data['descriptions_filepattern'] % int(puzzle_number)
@@ -55,7 +76,6 @@ def setuser():
         return make_response(render_template('logindenied.html', hideusername=True))
     else:
         players.append(name)
-        scores[name] = {}
         resp = make_response(render_template('loginconfirm.html', hideusername=True))
         resp.set_cookie('userID', name, secure=True)
         return resp
@@ -63,7 +83,8 @@ def setuser():
 @app.route('/getuser')
 def getuser():
     name = request.cookies.get('userID')
-    print(scores[name])
+    scores = db.session.query(Score).filter(Score.player_name == name)
+    print(scores)
     return '<p>username: '+name+'</p>'
 
 @app.route('/startacceptinganswers')
@@ -80,20 +101,17 @@ def stop():
 
 @app.route('/resetscoresandplayers')
 def reset():
-    global players
-    global scores
     players = []
-    scores = {}
+    # scores can not be reset this way...
     return render_template("confirm.html", hideusername=True)
 
 @app.route('/myscore')
 def myscore():
     name = request.cookies.get('userID')
-    if (name not in scores):
-        scores[name] = {}
     if (name not in players):
         players.append(name)
-    return render_template("myscore.html", username=name, myscore=scores[name])
+    myscore = db.session.query(Score).filter(Score.player_name == name)
+    return render_template("myscore.html", username=name, myscore=myscore)
 
 @app.route("/puzzle/<number>")
 def puzzle(number):
@@ -113,11 +131,11 @@ def submit_answer(number):
     answered = request.form['answer_input']
 
     if (answered in answers.get(number)):
-        if (name not in scores):
-            scores[name] = {}
         if (name not in players):
             players.append(name)
-        scores[name][number] = datetime.now()
+        score = Score(name, number)
+        db.session.add(score)
+        db.session.commit()
         template = "answercorrect.html"
     else:
         template = "answerincorrect.html"
@@ -126,21 +144,32 @@ def submit_answer(number):
 
 @app.route("/scoreboard")
 def score_board():
+    matrix = {}
     leaderboard = {}
-    items = []
-    for name, solved_times in scores.items():
-        latest_solve = datetime.fromtimestamp(0)
-        for puzzle_number, solved_time in solved_times.items():
-            if (solved_time > latest_solve):
-                latest_solve = solved_time
-        leaderboard[name] = {"solvedcount": len(scores[name]), "date": latest_solve.strftime("%H:%M:%S.%f")}
-    
-    items = sorted(leaderboard.items(), key = sort_count_date)
-    return render_template("scoreboard.html", hideusername=True, autorefresh=True, leaderboard=items)
+    results = db.session.query(Score).order_by(Score.player_name, Score.puzzle_id).all()
 
-def sort_count_date(tup):
-    key, d = tup
-    return -d["solvedcount"], d["date"]
+    for r in results:
+        if r.player_name not in matrix:
+            matrix[r.player_name] = {}
+        matrix[r.player_name][r.puzzle_id] = True
+    
+    for player_name, solved in matrix.items():
+        stars = ""
+        count = 0
+        for puzzle_id in get_puzzle_range():
+            if puzzle_id in solved:
+                stars += "*"
+                count += 1
+            else:
+                stars += " "
+        leaderboard[player_name] = {"solvedcount": count, "stars": stars }
+    
+    # items = sorted(results, key = sort_count_date)
+    return render_template("scoreboard.html", hideusername=True, autorefresh=True, leaderboard=leaderboard.items())
+
+# def sort_count_date(tup):
+#     key, d = tup
+#     return -d["solvedcount"], d["date"]
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8080)
