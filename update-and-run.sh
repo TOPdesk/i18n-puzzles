412e078cb54b01aae4fd258382385833083e8faa
Martijn van Iersel <martijni@topdesk.com>
2024-08-14 09:03:19 +0200

    Add missing dependency. Rename docker image to i18n-puzzles

--- update-and-run.sh
+++ update-and-run.sh
@@ -6,14 +6,14 @@ VERSION=1.2.0
 docker build -t ${IMAGE}:${VERSION} .
 docker tag ${IMAGE}:${VERSION} ${IMAGE}
 
-docker stop i18n-challenges
-docker rm i18n-challenges
+docker stop i18n-puzzles
+docker rm i18n-puzzles
 docker run \
 	-p 5000:5000 \
 	-v /srv/code-challenge:/srv/code-challenge \
 	-d \
 	--restart always \
-	--name i18n-challenges \
+	--name i18n-puzzles \
 	--env-file ./.env \
 	amarillion/i18n-challenges
 
